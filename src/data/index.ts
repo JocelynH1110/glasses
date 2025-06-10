2cd17653c3fe3bd595d11c7212d2cc2225427bc5
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-06-11 02:55:56 +0800

    建立卡片式結構

---
+++ src/data/index.ts
@@ -0,0 +1,4 @@
+import type { Shop } from "../interfaces";
+import _shops from "./shops.json";
+
+export const shops = _shops as Shop[];
---
+++ src/interfaces/shops.ts
@@ -0,0 +1,7 @@
+export interface Shop {
+  image: string;
+  name: string;
+  phone: string;
+  openingTimes: string;
+  address: string;
+}
