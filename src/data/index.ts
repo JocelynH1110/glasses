076d769460172aba24ea5c16f898a3a3753df91b
Jocelyn Huang <jocelyn.huang1110@gmail.com>
2025-06-12 01:06:48 +0800

    footer layout

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
 import type { Shop } from "../interfaces";
 import _shops from "./shops.json";
 
-export const shops = _shops as Shop[];
+export const shops = _shops as Shop[]; //Shop[] 做類型檢查，_shops、shops 區分資料來源和處理後的輸出
